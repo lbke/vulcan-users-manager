6c4bb4ffcac5fa914244580f08e8f8a3a798ea55
Eric Burel <e.burel.contact@gmail.com>
2018-09-11 15:54:45 +0200

    Initial commit

---
+++ package.js
@@ -0,0 +1,10 @@
+Package.describe({
+  name: "users-admin"
+});
+
+Package.onUse(api => {
+  api.use(["vulcan:core", "vulcan:users", "collection-admin", "menu"]);
+
+  api.mainModule("lib/server/main.js", "server");
+  api.mainModule("lib/client/main.js", "client");
+});
